83d6f34b639d0f7ce8316124459bf403f7e6129a
Logan Anderson <landerson2@upei.ca>
2021-01-03 16:45:25 -0400

    made a simple server

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from gpiozero import LED
+app = Flask(__name__)
+green = LED(3)
+red = LED(2)
+yellow = LED(4)
+
+
+def all_off():
+    yellow.off()
+    green.off()
+    red.off()
+
+
+all_off()
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/on/<color>')
+def update_color(color):
+    all_off()
+    if color == 'green':
+        green.on()
+    if color == 'yellow':
+        yellow.off()
+    if color == 'red':
+        red.off()
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
